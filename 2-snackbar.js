f490c816bd6ca3d515472581019978dbbbf3a4cc
OlhaIrhizova <irhizovaolha@gmail.com>
2024-11-28 14:31:42 +0000

    Deploying to gh-pages from @ OlhaIrhizova/goit-js-hw-10@c15987cdf40e944378ab95e70a62fb344a9ce6aa 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/styles-ZXNzG7EY.js";import{i as r}from"./assets/vendor-I1I71QQ2.js";const t=document.querySelector(".form");t.addEventListener("submit",a);function a(i){i.preventDefault();const e=parseInt(t.delay.value,10),o=t.state.value;new Promise((s,m)=>{setTimeout(()=>{o==="fulfilled"?s(e):m(e)},e)}).then(s=>{r.success({title:"Success",message:`✅ Fulfilled promise in ${s}ms`})}).catch(()=>{r.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})}
+//# sourceMappingURL=2-snackbar.js.map
